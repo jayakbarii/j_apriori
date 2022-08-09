ffbf3391eb57f2ac4afb278bc7fb6e8b04db9c94
jayakbarii <jayakbari96@.com>
2022-08-09 00:01:07 -0400

    Updated

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, url_for, request
 from csv import reader
 import io
+import time
 
 app = Flask(__name__)
 def getData(fname):
@@ -43,7 +44,7 @@ def generate(Ak, A):  # creates Ck
                     tList.append(Ak[i] | Ak[j])  # set union
     return tList
 
-def sD1(D, Ck, supp):
+def has_infrequent_subset(D, Ck, supp):
     ssdCnt = {}
     for tid in D:
         for can in Ck:
@@ -58,56 +59,48 @@ def sD1(D, Ck, supp):
     for key in ssdCnt:
         support = ssdCnt[key] / numItems * 1000
 
-        # support = ssdCnt[key]
-        # print(support)
-
         if support >= supp:
             retList.insert(0, key)
         sR[key] = support
     return (retList, sR)
 
-def readFile(fname, minSup):
+def apriori_gen(fname, minSup):
     (temp1, tempList) = getData(fname)
 
     # print(recordSetList)
 
-    (L1, sD) = sD1(tempList, temp1, minSup)
+    (L1, sD) = has_infrequent_subset(tempList, temp1, minSup)
     L = [L1]
     k = 2
     while len(L[k - 2]) > 0:
         Ck = generate(L[k - 2], k)
-        (Lk, supK) = sD1(tempList, Ck, minSup)  # scan DB to get Lk
+        (Lk, supK) = has_infrequent_subset(tempList, Ck, minSup)
         sD.update(supK)
         L.append(Lk)
         k += 1
     return (L, sD)
 
-def sorted(list1):
-    for temp in list1:
-        for i1 in list1[0]:
-            for i2 in list1[1]:
-                for i3 in list1[2]:
-                    for i4 in list1[3]:
-                        if i1.issubset(i2) \
-                                or i1.issubset(i3) \
-                                or i1.issubset(i4):
-                            try:
-                                list1[0].remove(i1)
-                            except:
-                                {}
-                        if i2.issubset(i3) \
-                                or i2.issubset(i4):
-                            try:
-                                list1[1].remove(i2)
-                            except:
-                                {}
-                        if i3.issubset(i4):
-                            try:
-                                list1[2].remove(i3)
-                            except:
-                                {}
-    return list1
+def find_frequent_1_itemsets (data):
+    tempFrequentData = []
+    for i in data:
+        for j in i:
+            tempFrequentData.append(j)
+    finalDataSets = []
+    for i in tempFrequentData:
+        finalDataSets.append(i)
 
+    for i in range(len(tempFrequentData)):
+        for j in range(i + 1, len(tempFrequentData)):
+            if tempFrequentData[i].issubset(tempFrequentData[j]):
+                try:
+                    if tempFrequentData[i] in finalDataSets:
+                        finalDataSets.remove(tempFrequentData[i])
+                except:
+                    {}
+    dataSet = []
+    for item in finalDataSets:
+        dataSet.append(list(item))
+    return dataSet
 
 
 @app.route('/')
@@ -120,13 +113,11 @@ def result():
     minSupp = request.form['minSupp']
     output = request.files['myfile']
     print(minSupp)
-    f1, rules = readFile(output,int(minSupp))
-    f1 = sorted(f1)
-    return render_template('first.html', temp = f1)
-    
-
-
-
+    start_time = time.time()
+    f1, rules = apriori_gen(output,int(minSupp))
+    f1 = find_frequent_1_itemsets (f1)
+    final = [f1, len(f1), output, minSupp, (time.time() - start_time)]
+    return render_template('first.html', temp = final)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
